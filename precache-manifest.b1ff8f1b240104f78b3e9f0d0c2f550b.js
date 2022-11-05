d6efab0285c9b89569889e693de08dac4ce1d084
rdx36 <dixitroshan79@gmail.com>
2022-11-05 16:45:21 +0530

    Updates

---
+++ precache-manifest.b1ff8f1b240104f78b3e9f0d0c2f550b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07a97b25d14dbc11eba450628497c1d0",
+    "url": "/boxoffice/index.html"
+  },
+  {
+    "revision": "61346e2baa879184ac46",
+    "url": "/boxoffice/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b2482944a2c8d3e42106",
+    "url": "/boxoffice/static/js/2.9cb010d9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/boxoffice/static/js/2.9cb010d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61346e2baa879184ac46",
+    "url": "/boxoffice/static/js/main.2933e8f2.chunk.js"
+  },
+  {
+    "revision": "2713fdea49f1beda18e0",
+    "url": "/boxoffice/static/js/runtime-main.0fb93b9d.js"
+  }
+]);
